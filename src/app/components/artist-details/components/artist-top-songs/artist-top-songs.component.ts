6b42ab47b96b513347a25b7a51e613fdcf42a4ad
Jared <jared@21digital.co.za>
2022-11-13 17:55:42 +0200

    Add ability to play clip preview and reformated HTML files

--- src/app/components/artist-details/components/artist-top-songs/artist-top-songs.component.ts
+++ src/app/components/artist-details/components/artist-top-songs/artist-top-songs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { Observable, Subscription, switchMap, throwError } from 'rxjs';
 
@@ -12,17 +12,21 @@ import { ArtistDetailsService } from 'src/app/services/artist-details.service';
 export class ArtistTopSongsComponent implements OnInit, OnDestroy {
     @Input() $route!: Observable<string>;
 
+    @ViewChild('audio_element', { static: true })
+    public audio_element!: ElementRef<HTMLAudioElement>;
+
     public top_songs: Track[] = [];
     public loading_top_songs: boolean = true;
+    public playing_song: boolean = false;
+    public current_audio_clip!: string;
     public placeholders: any[] = new Array(5);
 
     private top_songs_sub?: Subscription;
 
+
     constructor(
         private artistDetailsService: ArtistDetailsService
     ) { }
-
-    // TODO: add in audio tag so you can play track preview
 
     public ngOnInit(): void {
         this.top_songs_sub = this.$route
@@ -52,4 +56,27 @@ export class ArtistTopSongsComponent implements OnInit, OnDestroy {
         this.top_songs_sub?.unsubscribe();
     }
 
+    public playSongPreview(url: string): void {
+        if (!this.playing_song) {
+            this.initAudioEl(url);
+            this.current_audio_clip = url;
+            this.audio_element.nativeElement.play();
+            this.playing_song = true;
+        } else {
+            if (url === this.current_audio_clip) {
+                this.audio_element.nativeElement.pause();
+                this.playing_song = false;
+            } else {
+                this.initAudioEl(url);
+                this.audio_element.nativeElement.play();
+                this.current_audio_clip = url;
+                this.playing_song = true;
+            }
+        }
+    }
+
+    private initAudioEl(url: string): void {
+        this.audio_element.nativeElement.src = url;
+        this.audio_element.nativeElement.load();
+    }
 }
