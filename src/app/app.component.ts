a6fe1a0532291f37e5b816a40ffb9e752ce014b8
Jared <jared@21digital.co.za>
2022-11-13 18:27:46 +0200

    Add AOS to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as AOS from 'aos';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    public ngOnInit(): void {
+        AOS.init();
+    }
+}
